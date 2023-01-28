b1f322b3e56dbe51b002d5c7e3925b3e7eed67ae
redpt1 <397240017@qq.com>
2023-01-28 12:35:35 +0800

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.baidu.com"
           target="_blank"
           rel="noopener noreferrer"
         >
